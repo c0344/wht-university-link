d8fbe1a4f1a77ea4aaf1e7439ffd499e3b5b1fc8
Lil'Makie <38677180+c0344@users.noreply.github.com>
2019-04-11 09:21:53 +0800

    Create 12806.js

---
+++ univ/12806.js
@@ -0,0 +1,20 @@
+var univ_list = {
+    school: "中山职业技术学院”",
+    id: "12806",
+    link: [ {
+        name: "中职主页",
+        url: "http://www.zspt.cn/"
+    }, {
+        name: "信息门户",
+        url: "http://imy.zspt.cn/login.portal"
+    }, {
+        name: "教学综合信息服务平台",
+        url: "http://ijw.zspt.cn/jwglxt"
+    }, {
+        name: "综合测评"
+        url: "http://xw.zspt.cn/ceping/myindex.action"
+    }, {
+        name: "超星慕课"
+        url: "http://zspt.fy.chaoxing.com/portal"
+    }]
+};
